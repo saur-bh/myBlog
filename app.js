32f2c8331972ca344e9c5f6e05f3076768456154
Saurabh Verma <saurabh-verma@outlook.com>
2020-09-21 19:21:00 +0530

    Final version with compose, readMore

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+// Load the full build ofr lodash : npm i --save lodash 
+var _ = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -22,6 +24,30 @@ var posts=[];
 app.get('/',function(req,res){
   
   res.render("home",{startingConent:homeStartingContent,posts:posts});
+
+  
+});
+
+// get specific post
+
+app.get('/posts/:pos',function(req,res){
+  
+  var requestTitle = req.params.pos; 
+  mrequestTitle = _.lowerCase(requestTitle);
+  
+
+  posts.forEach(post=>{
+   
+   var  mpostTitle = _.lowerCase(post.title) ;
+
+    if(mpostTitle===mrequestTitle){
+
+      res.render("post",{reqTitle:post.title, reqContent:post.content });
+    }
+    
+
+  });
+  
 
   
 });
